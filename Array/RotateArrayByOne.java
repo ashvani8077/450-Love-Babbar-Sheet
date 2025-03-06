cb227f07791ac4665ea205f77a2ed69c81ae5935
Ashvani Sharma <panditashuabvp@gmail.com>
2025-03-07 00:45:34 +0530

    Rotate Array by 1

---
+++ Array/RotateArrayByOne.java
@@ -0,0 +1,16 @@
+public class RotateArrayByOne {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        rotate(arr);
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+    }
+    public static void rotate(int arr[]){
+        int temp=arr[arr.length-1];
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0]=temp;
+    }
+}
